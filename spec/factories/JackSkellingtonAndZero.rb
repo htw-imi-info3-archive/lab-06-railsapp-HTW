e2a9d64f3f3c2062f728e836096dafe02b69a18a
EkoSHtw <E.Supardjo@gmx.de>
2016-01-08 12:04:55 +0100

    Changes to be committed: 	new file:   JackSkellingtonAndZero.rb

---
+++ spec/factories/JackSkellingtonAndZero.rb
@@ -0,0 +1,13 @@
+factory :Jack Skellington, class: Pumpkin do # by Eko Supardjo s0550458
+  name 'King of Pumpkin'
+  color 'Black and White'
+  size  'l'
+  price '80.00'
+end
+
+factory :Zero, class: Pumpkin do # by Eko Supardjo s0550458
+  name 'Zero'
+  color 'White with blue shine and a red shiny spot'
+  size  's'
+  price '10.00'
+end
