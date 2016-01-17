d416019d0964549bdf29015347fd081a7bde6373
Natalie Grasser <mm@NGs-MacBook-Pro.home>
2016-01-17 19:30:56 +0100

    Lab 06, Part 2 & 3 bearbeitet

--- spec/features/customer_creates_order_spec.rb
+++ spec/features/customer_creates_order_spec.rb
@@ -9,6 +9,7 @@ describe "customer page", :type => :feature do
     visit "/customers/#{@customer.id}"
     expect(page).to have_link 'New Order'
   end
+
   it "initializes new order correctly" do
     visit "/customers/#{@customer.id}"
     click_link 'New Order'
@@ -17,4 +18,18 @@ describe "customer page", :type => :feature do
     order = Order.last
     expect(order.customer).to eq @customer
   end
+
+#Lab 06 - Part 2
+  it "show all orders" do
+    visit "/customers/#{@customer.id}"
+    expect(page).to have_content (:order)
+  end
+
+#Lab06 - Part 3
+  it "total price"
+    visit "/customers/#{@customer.id}"
+    sum = @customer.orders.sum(:price)
+    expect(page).to have_content (sum)
+  end
+
 end
